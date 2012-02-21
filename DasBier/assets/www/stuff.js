be76bd79f129fa23127caa2c326b74030b5472bf
ameliachase <theamelianator@gmail.com>
2012-02-21 11:02:08 -0600

    icons

---
+++ DasBier/assets/www/stuff.js
@@ -0,0 +1,55 @@
+<script language = "javascript">
+var toolbar = new Ext.Toolbar({
+    dock : 'top',
+    title: 'Biergarten'
+});
+
+var tabPanel = new Ext.TabPanel({
+    dock: 'top',
+    styleHtmlContent: true,
+    tabBar: {
+        dock: 'top',
+        layout: {
+            pack: 'center'
+        }
+    },
+    defaults: {
+        scroll: 'vertical'
+    },
+    items: [{
+        title: 'Das Bier',
+        html: 'Dis is where die beer goes',
+        
+        
+    },
+    {
+        title: 'Das Buddies',
+        html: 'Who needs friends?',
+        
+    },
+    {
+        title: 'Die Bars',
+        html: 'get mo beer',
+       
+    },
+    {
+        title: 'Settings',
+        html: 'I guess we can have this',
+        
+    }]
+});
+
+new Ext.Application({
+    launch: function() {
+        new Ext.Panel({
+            fullscreen: true,
+            dockedItems: [
+                toolbar,
+                tabPanel
+            ],
+            layout: 'fit',
+            scroll: 'vertical'
+        });
+    }
+});
+</script>​
--- DasBier/src/com/denton/biergarten/BierGartenActivity.java
+++ DasBier/src/com/denton/biergarten/BierGartenActivity.java
@@ -1,6 +1,7 @@
 package com.denton.biergarten;
 
 //import android.R;
+import android.R.drawable;
 import android.app.TabActivity;
 import android.content.Intent;
 import android.content.res.Resources;
@@ -13,7 +14,6 @@ public class BierGartenActivity extends TabActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-
         Resources res = getResources(); // Resource object to get Drawables
         TabHost tabHost = getTabHost();  // The activity TabHost
         TabHost.TabSpec spec;  // Reusable TabSpec for each tab
@@ -22,20 +22,26 @@ public class BierGartenActivity extends TabActivity {
 
         // Create an Intent to launch an Activity for the tab (to be reused)
         intent = new Intent().setClass(this, DasBierActivity.class);
+       
+      
 
         // Initialize a TabSpec for each tab and add it to the TabHost
-        spec = tabHost.newTabSpec("bier").setIndicator("Das Bier")
+        spec = tabHost.newTabSpec("bier").setIndicator("Beers",
+        		res.getDrawable(R.drawable.beerlong))
                       .setContent(intent);
+        
         
         tabHost.addTab(spec);
 
         intent = new Intent().setClass(this, DasPlacesActivity.class);
-        spec = tabHost.newTabSpec("places").setIndicator("Das Places")
+        spec = tabHost.newTabSpec("places").setIndicator("Places",
+        		res.getDrawable(R.drawable.placeslong))
                       .setContent(intent);
         tabHost.addTab(spec);
         
         intent = new Intent().setClass(this, DasBuddyActivity.class);
-        spec = tabHost.newTabSpec("buddies").setIndicator("Das Buddies")
+        spec = tabHost.newTabSpec("buddies").setIndicator("Buddies",
+        		res.getDrawable(R.drawable.beerbuddylong))
                       .setContent(intent);
         tabHost.addTab(spec);
 
