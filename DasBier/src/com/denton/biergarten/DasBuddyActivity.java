886819f5f389096857dace8c38b1214a62d4e680
ameliachase <theamelianator@gmail.com>
2012-02-23 14:00:26 -0600

    Das buddy skeleton

--- DasBier/src/com/denton/biergarten/DasBuddyActivity.java
+++ DasBier/src/com/denton/biergarten/DasBuddyActivity.java
@@ -7,9 +7,12 @@ import com.phonegap.*;
 
 public class DasBuddyActivity extends DroidGap {
     /** Called when the activity is first created. */
+	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         super.loadUrl("file:///android_asset/www/buddy.html");
+        
+        
     }
 }
