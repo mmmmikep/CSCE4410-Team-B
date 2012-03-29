6e93cd9e594c1755ec79cf0cf47e4955908aa2d1
Zach <zach@172.18.195.211>
2012-03-29 15:25:48 -0500

    working on popup screen and saving it to shared prefrences

--- DasBier/src/com/denton/biergarten/BierGartenActivity.java
+++ DasBier/src/com/denton/biergarten/BierGartenActivity.java
@@ -1,18 +1,88 @@
 package com.denton.biergarten;
 
 import android.app.TabActivity;
+import android.content.DialogInterface.OnClickListener;
 import android.content.Intent;
 import android.content.res.Resources;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.view.ViewGroup.LayoutParams;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.PopupWindow;
 import android.widget.TabHost;
+import android.widget.TextView;
 
 public class BierGartenActivity extends TabActivity {
     /** Called when the activity is first created. */
-    @Override
+	
+	///////3/29
+    PopupWindow popUp;
+    LinearLayout layout;
+    TextView tv;
+    LayoutParams params;
+    LinearLayout mainLayout;
+    Button but;
+    boolean click = true;
+    /////3/29
+	
+	
+	
+	
+ /*   @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        setContentView(R.layout.main);*/
+        
+        ////////////////////////3/29
+        
 
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+         super.onCreate(savedInstanceState);
+         popUp = new PopupWindow(this);
+         layout = new LinearLayout(this);
+         mainLayout = new LinearLayout(this);
+         tv = new TextView(this);
+         but = new Button(this);
+         but.setText("Click Me");
+         but.setOnClickListener(new View.OnClickListener() {
+
+          public void onClick(View v) {
+           if (click) {
+            popUp.showAtLocation(layout, Gravity.BOTTOM, 10, 10);
+            popUp.update(50, 50, 300, 80);
+            click = false;
+           } else {
+            popUp.dismiss();
+            click = true;
+           }
+          }
+
+         });
+         params = new LayoutParams(LayoutParams.WRAP_CONTENT,
+           LayoutParams.WRAP_CONTENT);
+         layout.setOrientation(LinearLayout.VERTICAL);
+         tv.setText("Hi this is a sample text for popup window");
+         layout.addView(tv, params);
+         popUp.setContentView(layout);
+         // popUp.showAtLocation(layout, Gravity.BOTTOM, 10, 10);
+         mainLayout.addView(but, params);
+         setContentView(mainLayout);
+        }
+}
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        /////////////////////////////////////3/29 end
+/*
         Resources res = getResources(); // Resource object to get Drawables
         TabHost tabHost = getTabHost();  // The activity TabHost
         TabHost.TabSpec spec;  // Reusable TabSpec for each tab
@@ -43,4 +113,4 @@ public class BierGartenActivity extends TabActivity {
 
         tabHost.setCurrentTab(0);
     }
-}
+}*/
--- DasBier/src/com/denton/biergarten/DasBierActivity.java
+++ DasBier/src/com/denton/biergarten/DasBierActivity.java
@@ -2,22 +2,48 @@ package com.denton.biergarten;
 
 //import android.app.Activity;
 import android.app.Activity;
+import android.content.DialogInterface.OnClickListener;
+import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.webkit.WebView;
-//trying to create a new branch
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class DasBierActivity extends Activity {
     /** Called when the activity is first created. */
     
-    WebView mWebView;
-    
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.bier);
+  
+	private SharedPreferences UandP;
+	public static final String PREFERENCE_FILENAME = "UandPStuff";
+	
+	public void onCreate(Bundle savedInstanceState) {
+	    super.onCreate(savedInstanceState);
+	    UandP = this.getSharedPreferences("PREFERENCE_FILENAME", MODE_PRIVATE);
+	    if(UandP.getString("username", null) != null) {
+	        setContentView(R.layout.main);
+	            // do some stuff...
+	    }
+	    else {
+	            setContentView(R.layout.popupuserpass);
+	            final EditText editText1 = (EditText) findViewById(R.id.editText1);
+	            final EditText editText2 = (EditText) findViewById(R.id.editText2);
+	            
+	            Button buttonA = (Button) findViewById(R.id.button1);
 
-        mWebView = (WebView) findViewById(R.id.webview);
-        mWebView.getSettings().setJavaScriptEnabled(true);
-        mWebView.loadUrl("file:///android_asset/www/bier.html");
-    }
+	            buttonA.setOnClickListener(new View.OnClickListener() {
+	            	
+	                @Override
+	                public void onClick(View v) {
+	                    SharedPreferences.Editor UandPEditor = UandP.edit();
+	                    UandPEditor.putString("username", editText1.getText().toString());
+	                    UandPEditor.putString("password", editText2.getText().toString());
+	                    UandPEditor.commit();
+	                }
+
+	            }); 
+	                    // do some other stuff...
+	    }
+	}
+	
+	
 }
