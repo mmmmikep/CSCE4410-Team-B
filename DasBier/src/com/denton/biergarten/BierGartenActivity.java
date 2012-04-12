dfbfb00c5d47e31a6e889754aaaac179e10d35fa
ameliachase <theamelianator@gmail.com>
2012-04-12 13:22:12 -0500

    git jiggy with it

--- DasBier/src/com/denton/biergarten/BierGartenActivity.java
+++ DasBier/src/com/denton/biergarten/BierGartenActivity.java
@@ -1,11 +1,11 @@
 package com.denton.biergarten;
 
 import android.app.TabActivity;
+
 import android.content.Intent;
 import android.content.res.Resources;
 import android.graphics.Color;
 import android.os.Bundle;
-import android.view.View;
 import android.webkit.WebView;
 import android.widget.TabHost;
 
@@ -21,15 +21,20 @@ public class BierGartenActivity extends TabActivity {
         tabHost.setBackgroundColor(Color.parseColor("#f5ad03"));//change default tab background to beer color
         tabHost.setBackgroundDrawable(getResources().getDrawable(R.drawable.bubblebeer));//set beer bubbles as image for tabs
         tabHost.setScrollBarStyle(WebView.SCROLLBARS_OUTSIDE_OVERLAY);
+       
         
         
         tabHost.setVerticalScrollBarEnabled(false);
+        
         TabHost.TabSpec spec;  // Reusable TabSpec for each tab
         Intent intent;  // Reusable Intent for each tab
-
+        
+        
+        
         // Create an Intent to launch an Activity for the tab (to be reused)
         intent = new Intent().setClass(this, DasBierActivity.class);
- 
+      
+
         
         // Initialize a TabSpec for each tab and add it to the TabHost
         spec = tabHost.newTabSpec("bier").setIndicator("Das Bier",
@@ -48,7 +53,18 @@ public class BierGartenActivity extends TabActivity {
                           res.getDrawable(R.drawable.beerbuddylong))
                       .setContent(intent);
         tabHost.addTab(spec);
-
+        
+        
         tabHost.setCurrentTab(0);
+       
+        
+          for(int i=0;i<tabHost.getTabWidget().getChildCount();i++)
+        {
+             tabHost.getTabWidget().getChildAt(i).setBackgroundColor(Color.TRANSPARENT);
+        }
+         tabHost.getTabWidget().getChildAt(tabHost.getCurrentTab()).setBackgroundColor(Color.TRANSPARENT);
+       
+
+        
     }
 }
