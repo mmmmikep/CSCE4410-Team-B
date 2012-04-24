b6f716251a22a1d6f7d5cdf9d62b399d4e025af1
mmmmikep <mmmmike.p@gmail.com>
2012-04-24 12:09:40 -0500

    bubbly

--- DasBier/src/com/denton/biergarten/BierGartenActivity.java
+++ DasBier/src/com/denton/biergarten/BierGartenActivity.java
@@ -1,31 +1,40 @@
 package com.denton.biergarten;
 
 import android.app.TabActivity;
+
 import android.content.Intent;
 import android.content.res.Resources;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.webkit.WebView;
 import android.widget.TabHost;
 
 public class BierGartenActivity extends TabActivity {
     /** Called when the activity is first created. */
     @Override
-    
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-        
-        
-        
-        
-        Resources res = getResources(); // Resource object to get Drawables
-        TabHost tabHost = getTabHost();  // The activity TabHost
-        TabHost.TabSpec spec;  // Reusable TabSpec for each tab
-        Intent intent;  // Reusable Intent for each tab
 
+        Resources res = getResources(); // Resource object to get Drawables
+        TabHost tabHost = getTabHost(); // The activity TabHost
+        tabHost.setBackgroundColor(Color.parseColor("#f5ad03"));//change default tab background to beer color
+        tabHost.setBackgroundDrawable(getResources().getDrawable(R.drawable.bubblebeer));//set beer bubbles as image for tabs
+        tabHost.setScrollBarStyle(WebView.SCROLLBARS_OUTSIDE_OVERLAY);
+       
+        
+        
+        tabHost.setVerticalScrollBarEnabled(false);
+        
+        TabHost.TabSpec spec; // Reusable TabSpec for each tab
+        Intent intent; // Reusable Intent for each tab
+        
+        
+        
         // Create an Intent to launch an Activity for the tab (to be reused)
         intent = new Intent().setClass(this, DasBierActivity.class);
+      
 
-            
         
         // Initialize a TabSpec for each tab and add it to the TabHost
         spec = tabHost.newTabSpec("bier").setIndicator("Das Bier",
@@ -40,11 +49,22 @@ public class BierGartenActivity extends TabActivity {
         tabHost.addTab(spec);
         
         intent = new Intent().setClass(this, DasBuddyActivity.class);
-        spec = tabHost.newTabSpec("buddies").setIndicator("Das Buddies",
+        spec = tabHost.newTabSpec("buddy").setIndicator("Das Buddies",
                           res.getDrawable(R.drawable.beerbuddylong))
                       .setContent(intent);
         tabHost.addTab(spec);
-
+        
+        
         tabHost.setCurrentTab(0);
+       
+        
+          for(int i=0;i<tabHost.getTabWidget().getChildCount();i++)
+        {
+             tabHost.getTabWidget().getChildAt(i).setBackgroundColor(Color.TRANSPARENT);
+        }
+         tabHost.getTabWidget().getChildAt(tabHost.getCurrentTab()).setBackgroundColor(Color.TRANSPARENT);
+       
+
+        
     }
 }
